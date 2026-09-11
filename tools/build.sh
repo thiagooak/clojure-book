08dc6f95239b6957dde5cdada23744edf5e3b57c
Thiago Carvalho <thiago.oak@gmail.com>
2026-09-11 20:25:52 -0300

    Add deploy step to workflow

--- tools/build.sh
+++ tools/build.sh
@@ -9,7 +9,7 @@ mkdir -p build
 # brew install asciidoctor && gem install rouge
 asciidoctor \
   --failure-level=WARN \
-  -o build/book.html \
+  -o build/index.html \
   book.adoc
 
-echo "Built build/book.html"
+echo "Built build/index.html"
